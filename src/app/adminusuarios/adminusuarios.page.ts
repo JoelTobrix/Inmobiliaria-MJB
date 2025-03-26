dc2742d72d94900ac22cb6d0ef53838e7c51dfc1
JoelTobrix <pepedariojoel007@gmail.com>
2025-03-26 11:46:54 -0500

    Diseño usuario

--- src/app/adminusuarios/adminusuarios.page.ts
+++ src/app/adminusuarios/adminusuarios.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AdminusuariosPageModule } from './adminusuarios.module';
+<<<<<<< HEAD
+=======
+import { ModalController } from '@ionic/angular';
+import { RegistroPage } from '../registro/registro.page';
+>>>>>>> 0e335c1 (Diseño muestra de usuarios)
 
 @Component({
   selector: 'app-adminusuarios',
@@ -9,7 +14,17 @@ import { AdminusuariosPageModule } from './adminusuarios.module';
 })
 export class AdminusuariosPage implements OnInit {
 
+<<<<<<< HEAD
   constructor() { }
+=======
+  constructor(private modalCtrl: ModalController) { }
+  
+  async Registro(){
+      const modal= await this.modalCtrl.create({
+        component: RegistroPage
+      }); return await modal.present();
+    }
+>>>>>>> 0e335c1 (Diseño muestra de usuarios)
 
   ngOnInit() {
   }
